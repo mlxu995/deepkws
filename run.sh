1d93843dee70d46ce02ef32f0be0b5277eb17c9b
mlxu <228311995@qq.com>
2021-10-22 20:26:26 +0800

    add ds-res17.yaml

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 python=/home/mlxu/anaconda3/envs/speechbrain/bin/python
-gpus=2
-use_gpu=2,3
+gpus=1
+use_gpu=2
 config=hparams/resnet15.yaml
+# config=hparams/ds-res17.yaml
 
-CUDA_VISIBLE_DEVICES=${use_gpu} ${python} -m torch.distributed.launch --nproc_per_node=${gpus} train.py ${config} --distributed_launch --distributed_backend='nccl'
+# CUDA_VISIBLE_DEVICES=${use_gpu} ${python} -m torch.distributed.launch --nproc_per_node=${gpus} train.py ${config} --distributed_launch --distributed_backend='nccl'
+CUDA_VISIBLE_DEVICES=${use_gpu} ${python} train.py ${config} 
